6b7252b40086d6db22907cf046a3af9a1ffb9d1b
asadullatariq123 <asadasad49987@gmail.com>
2026-07-01 14:06:30 +0530

    Remove old frontend

--- frontend-vite/src/services/api.js
+++ frontend-vite/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = import.meta.env.VITE_API_URL;
 
 export const sendEmergency = async (message) => {
   const response = await axios.get(`${API}/chat`, {
